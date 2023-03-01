0ae04a7a68a6afd571da0c6042abcedce2d7bf46
balenzano <123358270+balenzano@users.noreply.github.com>
2023-03-01 13:28:45 +0100

    Add files via upload

---
+++ lista dinamica.py
@@ -0,0 +1,7 @@
+a=int(input("quanti numeri vuoi inserire:"))
+lista=[]
+for i in range(0,a):
+    n=int(input("inserisci numero:"))
+    lista.append(n)
+print("sto stampando una lista:")
+print(lista)
